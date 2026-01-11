d762df582bf5a5c07427dab76793352aceeb7398
lovlygod <aporaartema@gmail.com>
2026-01-11 19:40:41 +0300

    now favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,22 @@ export const metadata: Metadata = {
   title: 'SkyGram',
   description:
     'Personal cloud storage for Telegram',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: ['/favicon.ico'],
+    apple: [
+      { url: '/apple-icon.png' },
+      { url: '/apple-icon-180x180.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ]
+  },
 };
 
 export default function RootLayout({
